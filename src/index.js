c849b80266dd0b2508542ee96bad8af8ddea3385
bbdk612 <smsmisha51@gmail.com>
2021-09-08 20:36:41 +0500

    React-components lesson

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// const el = React.createElement(
+//     'h1',
+//     null,
+//     'Hello World, ReactJS!'
+// );
+
+const AppList = () => {
+    return (
+        <ul>
+            <li>My first Element</li>
+            <li>My Second Element</li>
+        </ul>
+    )
+}
+
+const AppHeader = () => <h1>This is my Header</h1>;
 
 
+const App = () => { 
+    return (
+        <>    
+            <AppHeader />
+            <AppList />
+            <AppHeader />
+            <AppList />
+        </>
+    )
+}
+ReactDOM.render(<App />, document.getElementById('root'));
